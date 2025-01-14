113b6fdb6448679fe5566c17f981f46e592bb4c5
Tyler J. Cagle <tylerjcagle@gmail.com>
2025-01-13 23:06:21 -0700

    quick fix

--- src/components/home/_Home.tsx
+++ src/components/home/_Home.tsx
@@ -4,6 +4,9 @@ export const Home = () => {
   return (
     <section className="flex h-screen w-full items-center justify-center">
       <Canvas />
+      <h3 className="fixed bottom-[5%] left-1/2 -translate-x-1/2 -translate-y-1/2 italic mix-blend-screen">
+        2025
+      </h3>
       <div className="absolute z-10 flex flex-col">
         <h1>
           Off Brand<span className="ml-1 text-xl">®</span>
